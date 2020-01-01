1a8aafede6e2ebe27679b89bfae6092b49c3ef14
Michal Rawlik <mmrawlik@gmail.com>
2020-01-01 23:13:19 +0100

    Code cleanup.

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,4 +1,5 @@
 use plotters::prelude::*;
+use std::convert::TryFrom;
 use std::f64::consts::PI;
 use rustfft::num_complex::Complex;
 use rustfft::num_traits::Zero;
@@ -8,36 +9,36 @@ use rustfft::FFTplanner;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let period: f64 = 10e-6;
     let lambda = 26.95e-12;
+
+    let ny: usize = 600;
+    let nz: usize = 800;
+
     let talbot_distance = 2. * period.powi(2) / lambda;
-    let ny = 600;
-    let nz = 800;
     let dz = talbot_distance / nz as f64;
     let dy = period * 5. / ny as f64;
-    println!("The talbot_distance is {:?}m", talbot_distance);
     let fy = fftfreq(ny, dy);
 
-    // The initial wavefront
-    // let mut y = Array::range(0., (ny as f64) * dy, dy)
-    let y: Vec<Complex<f64>> = (0..ny)
+    // The initial wavefront - rectangular grating
+    let g0: Vec<Complex<f64>> = (0..ny)
         .map(|y| (y as f64) * dy)
         .map(|y| 0.5 * (1. + (2. * PI * y / period).cos().signum()))
         .map(|y| Complex::new(y, 0.))
         .collect();
-    // println!("{:?}", y);
 
+    // prepare the FFT package, also for the inverse transform
     let mut planner = FFTplanner::new(false);
     let fft = planner.plan_fft(ny);
     let mut iplanner = FFTplanner::<f64>::new(true);
     let ifft = iplanner.plan_fft(ny);
     let mut input: Vec<Complex<f64>> = vec![Complex::zero(); ny];
 
+    // where the carpet is stored
     let mut z: Vec<Vec<Complex<f64>>> = vec![vec![Complex::zero(); ny]; nz];
-
-    z[0].copy_from_slice(&y);
+    z[0].copy_from_slice(&g0);
 
     for iz in 1..nz {
         // fft
-        input.copy_from_slice(&z[iz - 1]);
+        input.copy_from_slice(&z[0]);
         fft.process(&mut input, &mut z[iz]);
 
         // processing in the Fourier space
@@ -45,7 +46,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             // normalization
             *v *= Complex::new(1.0 / (ny as f64), 0.0);
             // transfer function for the Fresnel free propagation
-            let ddz = dz;// * iz as f64;
+            let ddz = dz * iz as f64;
             *v *= Complex::new(0., 2.0 * PI / lambda * ddz).exp();
             *v *= Complex::new(0., -1. * PI * lambda * ddz * f.powi(2)).exp();
         }
@@ -67,8 +68,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .join("rust.png");
     println!("{:?}", image_path);
 
+
+    let nz = u32::try_from(nz)?;
+    let ny = u32::try_from(ny)?;
+
     let root =
-        BitMapBackend::new(&image_path, (800, 600)).into_drawing_area();
+        BitMapBackend::new(&image_path, (nz, ny)).into_drawing_area();
 
     root.fill(&White)?;
 
@@ -76,7 +81,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .margin(20)
         .x_label_area_size(10)
         .y_label_area_size(10)
-        .build_ranged(0..800, 0..600)?;
+        .build_ranged(0..nz, 0..ny)?;
 
     chart
         .configure_mesh()
@@ -86,11 +91,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let plotting_area = chart.plotting_area();
 
-    for xplot in 0..800 {
-        for yplot in 0..600 {
+    for xplot in 0..nz {
+        for yplot in 0..ny {
             plotting_area.draw_pixel(
-                (xplot as i32, yplot as i32),
-                &HSLColor(0.0, 0.0, z[xplot][yplot].norm()))?;
+                (xplot, yplot),
+                &HSLColor(0.0, 0.0, z[xplot as usize][yplot as usize].norm()))?;
         }
     }
 
